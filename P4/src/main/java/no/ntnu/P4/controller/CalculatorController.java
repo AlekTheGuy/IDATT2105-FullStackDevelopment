0a5e46907fedff48f9a00951ebd321060fa1d810
Aleksander Halvorsen Holthe <alekhh123@gmail.com>
2022-03-03 12:32:16 +0100

    Completed P4

--- P4/src/main/java/no/ntnu/P4/controller/CalculatorController.java
+++ P4/src/main/java/no/ntnu/P4/controller/CalculatorController.java
@@ -2,6 +2,8 @@ package no.ntnu.P4.controller;
 
 import no.ntnu.P4.model.Equation;
 import no.ntnu.P4.service.Calculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,24 +12,32 @@ import java.util.ArrayList;
 @RestController
 @CrossOrigin
 public class CalculatorController {
+    Logger logger = LoggerFactory.getLogger(CalculatorController.class);
 
     @Autowired
     Calculator calculator;
 
     @GetMapping("/calculate")
     public String getEquationAnswer() {
+        logger.info("Last answer returned");
         return calculator.getCalculations().get(calculator.getCalculations().size() - 1);
     }
 
     @GetMapping("/calculate/all")
     public ArrayList<String> getCalculations() {
+        logger.info("All calculations returned");
         return calculator.getCalculations();
     }
 
     @PostMapping("/calculate")
     public Equation postEquation(@RequestBody Equation equation) {
-        String sum = calculator.calculate(equation.getNum1(), equation.getNum2(), equation.getOperator());
-        calculator.getCalculations().add(sum);
+        try {
+            String sum = calculator.calculate(equation.getNum1(), equation.getNum2(), equation.getOperator());
+            calculator.getCalculations().add(sum);
+            logger.info("Calculation added");
+        } catch (Exception e) {
+            logger.warn("Error: " + e.getMessage());
+        }
         return equation;
     }
 }
